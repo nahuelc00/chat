4bdd759009216035f6a77397b904f60c360275d3
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2024-03-06 18:36:19 -0300

    Create firebase interface

---
+++ frontend/src/interfaces/firebase-interfaces.ts
@@ -0,0 +1,12 @@
+type FirebaseConfiguration = {
+	apiKey: string;
+	authDomain: string;
+	projectId: string;
+	storageBucket: string;
+	messagingSenderId: string;
+	appId: string;
+	databaseURL: string;
+};
+
+export type {FirebaseConfiguration};
+
